6d064e5ee57ab7b584eb1e2d3e5a76bf80b1b8cf
SMOLKEYS.exe <75618732+SMOLKEYS@users.noreply.github.com>
2021-06-29 23:21:13 +0800

    our code

--- scripts/content/tomfoolery.js
+++ scripts/content/tomfoolery.js
@@ -1,2 +1,17 @@
-Events.on(ClientLoadEvent,e=>{Call.infoMessage("tomfoolery")})
+Events.on(ClientLoadEvent, b => {
+    Vars.ui.showCustomConfirm(
+        "tomfoolery",
+        "tomfoolery",
+        "haha",
+        "yes",
+        ()=>{
+            print("h");
+	    Call.infoMessage("no");
+        },
+        ()=>{
+	    Call.infoMessage("no");
+        }
+    );
+});
+//code above belongs to RebornTrack970, code below is created by me
 Core.bundle.debug("never gonna give you up, never gonna let you down")
