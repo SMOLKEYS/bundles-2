28fd1fa55f813d8215e86094d3f8db3af256cdf1
SMOLKEYS.exe <75618732+SMOLKEYS@users.noreply.github.com>
2021-06-30 21:29:01 +0800

    Update tomfoolery.js

--- scripts/content/tomfoolery.js
+++ scripts/content/tomfoolery.js
@@ -11,6 +11,5 @@ Vars.ui.showCustomConfirm(
 	    Call.infoMessage("no");
         }
     );
-});
 //code above belongs to RebornTrack970, code below is created by me
 Core.bundle.debug("never gonna give you up, never gonna let you down")
