5ea9d51990b3980cce1db55791a3510a7295c8b0
SMOLKEYS.exe <75618732+SMOLKEYS@users.noreply.github.com>
2021-06-30 21:30:42 +0800

    Update tomfoolery.js

--- scripts/content/tomfoolery.js
+++ scripts/content/tomfoolery.js
@@ -1,4 +1,5 @@
-Vars.ui.showCustomConfirm(
+Events.on(ClientLoadEvent, b => {
+      Vars.ui.showCustomConfirm(
         "you disgust me",
         "what kind of tomfoolery did you download",
         "idk",
@@ -11,5 +12,6 @@ Vars.ui.showCustomConfirm(
 	    Call.infoMessage("no");
         }
     );
+});
 //code above belongs to RebornTrack970, code below is created by me
 Core.bundle.debug("never gonna give you up, never gonna let you down")
